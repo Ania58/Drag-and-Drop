387f92bd1359acc6e07047d621798747cb93fb22
Anna Maria Heliasz <annemaryheliasz@gmail.com>
2025-05-18 17:54:19 +0200

    Refactor ProjectState into state/project-state.ts and update import

--- src/app.ts
+++ src/app.ts
@@ -1,56 +1,9 @@
 /// <reference path="models/drag-drop.ts" />
 /// <reference path="models/project.ts" />
+/// <reference path="state/project-state.ts" />
 
 namespace App {
     
-
-    type Listener<T> = (items: T[]) => void;
-
-    class State<T> {
-        protected listeners: Listener<T>[] = [];
-        addListener(listenerFn: Listener<T>) {
-            this.listeners.push(listenerFn);
-        }
-    }
-
-    class ProjectState extends State<Project>{
-        private projects: Project[] = [];
-        private static instance: ProjectState;
-
-        private constructor() {
-            super();
-        }
-
-        static getInstance() {
-            if (this.instance) {
-                return this.instance;
-            }
-            this.instance = new ProjectState();
-            return this.instance;
-        }
-
-        addProject(title: string, description: string, numOfPeople: number) {
-            const newProject = new Project(Math.random().toString(), title, description, numOfPeople, ProjectStatus.Active)
-            this.projects.push(newProject);
-            this.updateListeners();
-        }
-        moveProject(projectId: string, newStatus: ProjectStatus) {
-            const project = this.projects.find(prj => prj.id === projectId);
-            if (project && project.status !== newStatus) {
-                project.status = newStatus;
-                this.updateListeners();
-            }
-        }
-
-        private updateListeners() {
-            for (const listenerFn of this.listeners) {
-                listenerFn(this.projects.slice());
-            }
-        }
-    }
-
-    const projectState = ProjectState.getInstance();
-
     interface Validatable {
         value: string | number;
         required?: boolean;
