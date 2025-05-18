ef4c5d68e9ed7f8fef832b8ac121a4ae48cb8c47
Anna Maria Heliasz <annemaryheliasz@gmail.com>
2025-05-18 17:57:40 +0200

    Move validation logic to util/validation.ts and update import

--- src/app.ts
+++ src/app.ts
@@ -1,38 +1,11 @@
 /// <reference path="models/drag-drop.ts" />
 /// <reference path="models/project.ts" />
 /// <reference path="state/project-state.ts" />
+/// <reference path="util/validation.ts" />
 
 namespace App {
     
-    interface Validatable {
-        value: string | number;
-        required?: boolean;
-        minLength?: number;
-        maxLength?: number;
-        min?: number;
-        max?: number;
-    }
-
-    function validate(validatableInput: Validatable) {
-        let isValid = true;
-        if (validatableInput.required) {
-            isValid = isValid && validatableInput.value.toString().trim().length !== 0;
-        }
-        if (validatableInput.minLength != null && typeof validatableInput.value ==='string') {
-            isValid = isValid && validatableInput.value.length >= validatableInput.minLength;
-        }
-        if (validatableInput.maxLength != null && typeof validatableInput.value ==='string') {
-            isValid = isValid && validatableInput.value.length <= validatableInput.maxLength;
-        }
-        if (validatableInput.min != null && typeof validatableInput.value === 'number') {
-            isValid = isValid && validatableInput.value >= validatableInput.min;
-        }
-        if (validatableInput.max != null && typeof validatableInput.value === 'number') {
-            isValid = isValid && validatableInput.value <= validatableInput.max;
-        }
-        return isValid;
-    }
-
+    
 
 
     function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
